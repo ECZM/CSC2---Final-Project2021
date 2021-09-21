34fcadc7c696db4e529d159ac7eab9eb76a30180
Ella M <ellam.@Ellas-Air.home>
2021-09-21 20:33:17 +1200

    Update c01_customer.py

--- c01_customer.py
+++ c01_customer.py
@@ -5,6 +5,17 @@
 #import tkinter so we can make a GUI
 from tkinter import *
 
+#Check the inputs are all valid
+def check_inputs ():
+    Label(main_window, text="               ") .grid(column=2,row=0)
+    #Check that customer name is not blank and specific for string, set error text if blank   
+    if len(entry_customer.get()) == 0 :
+        Label(main_window,fg="red", text="*Required") .grid(column=2,row=0)
+    elif (entry_customer.get().isdigit()) : 
+        if  int(entry_customer.get()) <=0 or  int(entry_customer.get()) >=0:
+            Label(main_window,fg="red", text="*Invalid") .grid(column=2,row=0)
+       
+
 #create the buttons and labels
 def setup_buttons():
     #these are the global variables that are used
