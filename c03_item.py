db947e18463dd5543935376962498ce626b790ca
Ella M <ellam.@Ellas-Air.home>
2021-09-21 20:41:38 +1200

    Update c03_item.py

--- c03_item.py
+++ c03_item.py
@@ -23,11 +23,17 @@ def check_inputs ():
             Label(main_window,fg="red", text="*6 numbers only") .grid(column=2,row=1)
     else :
         Label(main_window,fg="red", text="*Invalid") .grid(column=2,row=1)
+    #Check that item hired is not blank and specific for string, set error text if blank     
+    if len(entry_item.get()) == 0 :
+        Label(main_window,fg="red", text="*Required") .grid(column=2,row=2)
+    elif (entry_item.get().isdigit()) : 
+        if  int(entry_item.get()) <=0 or  int(entry_item.get()) >=0:
+            Label(main_window,fg="red", text="*Invalid") .grid(column=2,row=2)
 
 #create the buttons and labels
 def setup_buttons():
     #these are the global variables that are used
-    global entry_customer, entry_receipt,entry_item
+    global entry_customer, entry_receipt, entry_item
     #create all the empty and default labels, buttons and entry boxes. Put them in the correct grid location
     Label(main_window, text="Customer Name") .grid(column=0,row=0,sticky=E)
     entry_customer = Entry(main_window)
